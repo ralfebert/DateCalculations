fc8d14c40061e876e9152850a15a5e8b407ce909
Ralf Ebert <info@ralfebert.de>
2021-10-10 14:31:15 +0200

    added license

--- Sources/DateCalculations/Date+AddingComponent.swift
+++ Sources/DateCalculations/Date+AddingComponent.swift
@@ -1,31 +1,53 @@
+// MIT License
+//
+// Copyright (c) 2021 Ralf Ebert
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 import Foundation
 
 public extension Date {
     func adding(years: Int) -> Date {
-        return Calendar.current.date(byAdding: .year, value: years, to: self)!
+        Calendar.current.date(byAdding: .year, value: years, to: self)!
     }
 
     func adding(months: Int) -> Date {
-        return Calendar.current.date(byAdding: .month, value: months, to: self)!
+        Calendar.current.date(byAdding: .month, value: months, to: self)!
     }
 
     func adding(weeks: Int) -> Date {
-        return Calendar.current.date(byAdding: .weekOfYear, value: weeks, to: self)!
+        Calendar.current.date(byAdding: .weekOfYear, value: weeks, to: self)!
     }
 
     func adding(days: Int) -> Date {
-        return Calendar.current.date(byAdding: .day, value: days, to: self)!
+        Calendar.current.date(byAdding: .day, value: days, to: self)!
     }
 
     func adding(hours: Int) -> Date {
-        return Calendar.current.date(byAdding: .hour, value: hours, to: self)!
+        Calendar.current.date(byAdding: .hour, value: hours, to: self)!
     }
 
     func adding(minutes: Int) -> Date {
-        return Calendar.current.date(byAdding: .minute, value: minutes, to: self)!
+        Calendar.current.date(byAdding: .minute, value: minutes, to: self)!
     }
 
     func adding(seconds: Int) -> Date {
-        return Calendar.current.date(byAdding: .second, value: seconds, to: self)!
+        Calendar.current.date(byAdding: .second, value: seconds, to: self)!
     }
 }
